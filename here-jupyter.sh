878d7a763e6d4acaa13364008c9a44d1126c320d
Stefaan Lippens <soxofaan@gmail.com>
2019-08-27 11:41:23 +0200

    Some finetuning

--- here-jupyter.sh
+++ here-jupyter.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# Build session name from current dir
+# Build tmux session name from current directory
 SESSION_NAME=$(pwd)
+# Strip non-alphanumeric characters
 SESSION_NAME=${SESSION_NAME//[^a-zA-Z0-9]/}
-SESSION_NAME=Jupiter-${SESSION_NAME: -20}
+# Only keep trailing part
+SESSION_NAME=Jupyter-${SESSION_NAME: -20}
 
-# Command to run inside tmux session
-# based on https://github.com/conda/conda/issues/7980
-COMMAND='eval "$(conda shell.bash hook)"; conda activate jupyter; jupyter notebook; sleep 5'
+# Build command to run inside tmux session
+CONDA_ENV=jupyter
+# Anaconda environment activation is based on https://github.com/conda/conda/issues/7980
+COMMAND='eval "$(conda shell.bash hook)";'
+COMMAND+="conda activate $CONDA_ENV;"
+COMMAND+='jupyter notebook;'
+# Sleep a bit to allow capturing of error messages if things go wrong
+COMMAND+='sleep 2;'
 
 echo "Starting tmux session '$SESSION_NAME' in background with command $COMMAND" 
 if tmux new-session -s $SESSION_NAME -d "$COMMAND"; then
-	echo "tmux launched with initial output:"
-	sleep 1
-	tmux capture-pane -p -t $SESSION_NAME
-	echo "to re-attach:"
-	echo "    tmux attach -t $SESSION_NAME"
+    echo "Tmux session launched with initial output:"
+    sleep 1
+    tmux capture-pane -p -t $SESSION_NAME
+    echo "To re-attach:"
+    echo "    tmux attach -t $SESSION_NAME"
 else
-	echo "Failed to launch tmux"
+    echo "Failed to launch tmux"
 fi
+
